e316169f58e86db3fe7662096de0f459801a1c18
Andrew Dixon <dixon@atomicbroadcast.net>
2013-11-14 10:04:39 -0800

    Finished Kata

--- lib/calculatoradvanced.rb
+++ lib/calculatoradvanced.rb
@@ -1,2 +1,50 @@
 class Calculatoradvanced
+  attr_reader :expr
+
+  def expr=(expression='')
+    @expr = expression
+    validate_expression
+  end
+
+  alias_method :initialize, :expr=
+
+  def add
+    @values.inject(&:+)
+  end
+
+  def diff
+    @values.inject(&:-)
+  end
+
+  def prod
+    @values.inject(&:*)
+  end
+
+  def div
+    @values.inject(&:/)
+  end
+
+  private
+
+  def validate_expression
+    if @expr =~ /,\n/ || @expr =~ /\n,/
+      raise "Consecutive Delimiters"
+    end
+
+    split_string = ",\n"
+
+    if match = @expr.match(/\/\/([^\n]*)\n(.*)/m)
+      delimeters, @expr = match.captures
+      delimeters = delimeters.split(/[\[\]]/)
+      delimeters.reject! { |delimeter| delimeter == '' }
+      split_string = "#{Regexp.escape(delimeters.join)}" + split_string
+    end
+
+    @values = @expr.split(/[#{split_string}]/).map(&:to_i)
+
+    @negatives = @values.select { |value| value < 0 }
+    if (@negatives && @negatives.any?)
+      raise "negatives not allowed: #{@negatives.join(', ')}"
+    end
+  end
 end
--- spec/calculatoradvanced_spec.rb
+++ spec/calculatoradvanced_spec.rb
@@ -2,11 +2,81 @@ require 'spec_helper'
 require 'calculatoradvanced'
 
 describe Calculatoradvanced do
-  describe "#initialize" do
-    it "instantiates" do
-      expect {
-        Calculatoradvanced.new
-      }.to_not raise_exception
+  subject(:calc) { Calculatoradvanced.new(expression) }
+  context 'with an empty expression' do
+    let(:expression) { '' }
+    specify { expect { calc }.to_not raise_exception }
+  end
+  context 'with expression "1\n2\n3"' do
+    let(:expression) { "1\n2\n3" }
+
+    specify { expect { calc }.to_not raise_exception }
+    its(:add) { should eq(6) }
+  end
+  context 'with expression "2,3\n4"' do
+    let(:expression) { "2,3\n4" }
+
+    its(:add) { should eq(9) }
+    its(:expr) { should eq("2,3\n4") }
+  end
+  context 'with expression "1,\n2"' do
+    let(:expression) { "1,\n2" }
+
+    specify { expect { calc.add }.to raise_exception }
+  end
+  context 'with expression "1\n,2"' do
+    let(:expression) { "1\n,2" }
+
+    specify { expect { calc.add }.to raise_exception }
+  end
+  shared_examples_for 'expression validation' do
+    specify { expect { method }.to raise_exception(RuntimeError, /negatives not allowed/) }
+    specify { expect { method }.to raise_exception(RuntimeError, /-1, -2, -3/) }
+  end
+  context 'with expression "-1,-2\n-3"' do
+    let(:expression) { "-1,-2\n-3" }
+    context '.add' do
+      let(:method) { calc.add }
+
+      it_behaves_like 'expression validation'
     end
+    context '.diff' do
+      let(:method) { calc.diff }
+      it_behaves_like 'expression validation'
+    end
+    context '.prod' do
+      let(:method) { calc.prod }
+      it_behaves_like 'expression validation'
+    end
+  end
+  context 'with expression "//[;]\n1;2"' do
+    let(:expression) { "//[;]\n1;2" }
+
+    its(:add) { should eq(3) }
+  end
+  context 'with expression "//[;]\n2;1"' do
+    let(:expression) { "//[;]\n2;1" }
+    its(:diff) { should eq(1) }
+    its(:prod) { should eq(2) }
+  end
+  context 'with expression "//[;]\n3;2"' do
+    let(:expression) { "//[;]\n3;2" }
+
+    its(:div) { should eq(1) }
+  end
+  context 'with expression "//[*][;]\n1*2;3"' do
+    let(:expression) { "//[*][;]\n1*2;3" }
+    its(:add)  { should eq(6)  }
+    its(:diff) { should eq(-4) }
+    its(:prod) { should eq(6)  }
+    its(:div)  { should eq(0)  }
+  end
+  context 'with expression "//[*][;][#]\n5*4;3#2"' do
+    let(:expression) { "//[*][;][#]\n5*4;3#2" }
+    its(:diff) { should eq(-4) }
+  end
+  context 'with expression "//[#][;][*]\n1*2#3;4,5\n6"' do
+    let(:expression) { "//[#][;][*]\n1*2#3;4,5\n6" }
+    its(:add) { should eq(21) }
   end
 end
